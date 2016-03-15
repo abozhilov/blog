0df41fc9cf141daf97820dbd9c0626c8c0b750e8
abozhilov <asen.bozhilov@gmail.com>
2016-03-15 23:58:54 +0200

    Update ArticleListView

--- articles/views.py
+++ articles/views.py
@@ -3,34 +3,39 @@ from django.views import generic
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from articles.models import Category, Tag, Article, Comment
 
-class ArticleView(generic.TemplateView):
+class BaseArticleView(generic.TemplateView):
     
     def get_context_data(self, **kwrds):
-        context = super(ArticleView, self).get_context_data(**kwrds)
+        context = super(BaseArticleView, self).get_context_data(**kwrds)
         context['categories'] = Category.objects.all()
         
         return context
 
 
-class ArticleListView(ArticleView):
+class ArticleListView(BaseArticleView):
     template_name = 'articles/list.html'
     
     def get_context_data(self, **kwrds):
         context = super(ArticleListView, self).get_context_data(**kwrds)
-        paginator = Paginator(Article.objects.all(), 10)
-        page_id = kwrds['page']
+        paginator = Paginator(self.get_paginator_data(), 10)
+        page = self.get_page(int(kwrds['page']), paginator.num_pages)
         
-        try:
-            context['articles'] = paginator.page(page_id)
-        except PageNotAnInteger:
-            context['articles'] = paginator.page(1)
-        except EmptyPage:
-            context['articles'] = paginator.page(paginator.num_pages)
-        
-        context['curr_page'] = page_id
+        context['articles']  = paginator.page(page)
+        context['curr_page'] = page
         context['last_page'] = paginator.num_pages
         
         return context
+        
+    def get_paginator_data(self):
+        return Article.objects.all()
+    
+    def get_page(self, page, max_page):
+        if page is None:
+            return 1
+        elif page > max_page:
+            return max_page
+        else:
+            return page
     
     
 
